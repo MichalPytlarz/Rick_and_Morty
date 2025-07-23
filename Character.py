398269a99f08d4bbc287b4402bbe42fdc4d42bdc
MichalPytlarz <bakm56345@gmail.com>
2025-07-23 21:49:01 +0200

    final_update

--- Character.py
+++ Character.py
@@ -1,3 +1,8 @@
+import os.path
+import io
+import requests
+from PIL import Image
+
 class Character:
     def __init__(self, name, gender, species, origin, status, image_url, number_of_episodes):
         self.name = name
@@ -7,6 +12,24 @@ class Character:
         self.status = status
         self.image_url = image_url
         self.number_of_episodes = number_of_episodes
+        self.image_name = self.name.replace(" ", "")+'.png'
+        self.image_path = './images/'+self.image_name
+        self.download_image()
 
     def show_character(self):
         print(self.name, self.gender)
+    
+    def download_image(self):
+        if not os.path.exists(self.image_path):
+            response = requests.get(self.image_url)
+            img_data = response.content
+
+            image = Image.open(io.BytesIO(img_data))
+            image = image.resize((200, 200), Image.Resampling.LANCZOS) # Lanczos gives better scaling IMG quality
+            image.save(self.image_path)
+
+            print(self.name + "Is downloaded!")
+
+    def get_image(self):
+        return Image.open(self.image_path)
+
--- Main.py
+++ Main.py
@@ -1,29 +1,83 @@
 import requests
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import ImageTk
 from Character import Character
+from Scrollable import ScrollableFrame
+
+def load_data():
+    url = "https://rickandmortyapi.com/api/character/?page=1"
+
+    response = requests.get(url)
+
+    json_response = response.json()
+    json_response_result = json_response["results"]
+
+    characters = []
+
+    for obj in json_response_result:
+        name = obj["name"]
+        gender = obj["gender"]
+        species = obj["species"]
+        origin = obj["origin"]["name"]
+        status = obj["status"]
+        image_url = obj["image"]
+        number_of_episodes = len(obj["episode"])
 
 
-url = "https://rickandmortyapi.com/api/character/?page=1"
+        character = Character(name, gender, species, origin, status, image_url, number_of_episodes)
 
-response = requests.get(url)
+        # character.show_character()
+        characters.append(character)
 
-json_response = response.json()
-json_response_result = json_response["results"]
+    return characters
 
-characters = []
-
-for obj in json_response_result:
-    name = obj["name"]
-    gender = obj["gender"]
-    species = obj["species"]
-    origin = obj["origin"]["name"]
-    status = obj["status"]
-    image_url = obj["image"]
-    number_of_episodes = len(obj["episode"])
+characters = load_data()
 
 
-    character = Character(name, gender, species, origin, status, image_url, number_of_episodes)
 
-    character.show_character()
-    characters.append(character)
+root = Tk()
+root.title("Rick and Morty Characters")
+root.geometry("535x560")
+root.update()
+root.resizable(0, 0)
+scrollable = ScrollableFrame(root)
+
+for char in characters:
+
+    # Frame for each character
+    list_item_frame = Frame(scrollable.scrollable_frame, borderwidth=4, relief=GROOVE)
+
+    # left frame for image
+    left_frame = Frame(list_item_frame)
+    # loading the img
+    photo = ImageTk.PhotoImage(char.get_image())
+    image_label = Label(left_frame, image = photo)
+    image_label.image = photo
+    image_label.pack(fill = BOTH, expand = True)
+    left_frame.grid(row = 0, column = 0, padx = 7.5, pady = 15)
+
+    # right frame for description 
+
+    right_frame = Frame(list_item_frame)
+
+    Properties = ["name", "species", "gender", "origin", "status"]
+
+    for i in range(len(Properties)):
+        prop = Properties[i]
+        Label(right_frame, text =f"{prop.capitalize()}: {getattr(char, prop)}" , font=("Calibri", 12), padx = 7.5).pack(anchor= W, expand= True)
+
+    Label(right_frame, text = str(char.number_of_episodes) + " Episodes", font=("Calibri", 12), padx = 7.5).pack(anchor= W, expand= True)
+
+
+
+    right_frame.grid(row = 0, column = 1, sticky = 'we')
+
+
+    list_item_frame.pack(fill = X, padx = 7.5)
+
+
+scrollable.pack(fill=BOTH, expand=True) 
+
+
+root.mainloop()
+
